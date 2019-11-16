76e520b4edfa3d6c0d02a9a6b502bbe7414338e7
minlwin <lwin.zawmin@gmail.com>
2019-11-16 18:55:24 +0630

    Commit Navigations

---
+++ 06.PassingDataNavigation/app/src/main/java/com/jdc/data/DetailsFragment.kt
@@ -0,0 +1,28 @@
+package com.jdc.data
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.fragment_details.*
+
+class DetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        arguments?.also {
+            arg -> output.text = DetailsFragmentArgs.fromBundle(arg).name
+        }
+    }
+
+}
---
+++ 06.PassingDataNavigation/app/src/main/java/com/jdc/data/EditFragment.kt
@@ -0,0 +1,35 @@
+package com.jdc.data
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_edit.*
+
+class EditFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_edit, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        button.setOnClickListener {
+            val name = input.text.toString()
+
+            if(name.isNotEmpty()) {
+                EditFragmentDirections.actionEditToDetails(name).apply {
+                    it.findNavController().navigate(this)
+                }
+            }
+        }
+    }
+}
